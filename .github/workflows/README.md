3e6989fe4b0008e1546e66ff43539ff9e15a8832
Indira Biyakhmetova <indira.051294@gmail.com>
2025-04-18 11:46:47 -0600

    Initial commit

---
+++ .github/workflows/README.md
@@ -0,0 +1,31 @@
+# TextNow Cypress Tests
+
+This project contains Cypress UI tests for the TextNow website.
+
+### ✅ Scope
+
+The tests currently cover a few key pages and focus primarily on UI functionality.
+
+### 🛠️ Tech Stack
+
+- [Cypress](https://www.cypress.io/) for end-to-end testing  
+- [cypress-real-events](https://github.com/dmtrKovalenko/cypress-real-events) to simulate more realistic user interactions
+
+### 🚀 Getting Started
+
+To run the tests locally:
+
+1. Clone the repository  
+2. Install dependencies:
+
+   ```bash
+   npm install
+
+3. Run Cypress tests:
+    To open Cypress Test Runner:
+        npx cypress open
+    To run tests headlessly (in the terminal):
+        npx cypress run
+
+🔄 Continuous Integration
+Tests are automatically run via GitHub Actions on every push or when manually triggered.
---
+++ cypress/e2e/navigation/get_free_number.cy.js
@@ -0,0 +1,51 @@
+
+
+describe('Navigation Menu - Get Free Number Dropdown', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('ul.links-nav > li').first().realHover();
+  })
+
+      it('Should display key features and buttons on the Free Phone Number page', () => {
+        cy.contains('Free Phone Number').click();
+        cy.url().should('include', '/free-phone-number');
+        cy.contains('h1', 'Get a free phone number. Start calling & texting today.').should('be.visible');
+        cy.contains('button', "Let's Do It").should('be.visible')
+        cy.contains('What comes with your free phone number?').should('be.visible')
+          .scrollIntoView()
+          .should('be.visible')
+        cy.contains('Unlimited talk & text').should('be.visible');
+        cy.contains('All your contacts').should('be.visible');
+        cy.contains('Caller ID & Voicemail').should('be.visible');
+        cy.get('button')
+          .filter(':visible') 
+          .filter((index, el) => el.innerText.trim() === 'Download the App')
+          .should('have.length', 3);  
+      })
+
+      it('Should search and display matching country', () => {
+        const countries = ['Australia', 'Germany', 'Canada', 'Switzerland', 'Kazakhstan', 'Japan', 'United States', 'Georgia', 'France', 'Sweden', 'Singapore', 'South Korea']
+        const randomCountry = countries[Math.floor(Math.random() * countries.length)]
+
+        cy.contains('International Calling').click();
+        cy.url().should('include', '/international-calling');
+        cy.contains('h1', 'Affordable International calling. Wallet-friendly rates.').should('be.visible');
+        cy.get('input[aria-label="Search for International Rates"]').type(randomCountry)
+        cy.get('ul li').contains(randomCountry)
+          .should('be.visible')
+      })
+
+      it('Should display message for invalid country search', () => {
+        const fakeCountries = ['Wakanda', 'Narnia', 'Jupiter', 'Atlantis', 'Moon'];
+
+        fakeCountries.forEach((fakeCountry) => {
+        cy.contains('International Calling').click();
+        cy.url().should('include', '/international-calling');
+        cy.contains('h1', 'Affordable International calling. Wallet-friendly rates.').should('be.visible');
+        cy.get('input[aria-label="Search for International Rates"]').clear().type(fakeCountry);
+        cy.get('h2').should('include.text', `Results for “${fakeCountry}”`);
+        cy.contains('div','No matching country could be found').should('be.visible')
+      })
+    })
+  })
+  
